471eb2f4544547663f6c54c3446b94f4eb41366e
Ahmad Muzaki <ahmadmuzaki@gmail.com>
2022-08-02 22:05:38 +0700

    copy dari github

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var io       = require('socket.io')(server);
 // Config
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req,res,next){
-    req.io = io;
-    next();
+	req.io = io;
+	next();
 });
 
-// Static files path
+// Tell express where to serve static files from
 app.use(express.static(__dirname + '/public'));
 
 // Connect to DB
@@ -22,42 +22,46 @@ mongoose.connect("mongodb://127.0.0.1:27017/realtime_chart");
 var schema = mongoose.Schema({name: String});
 var Vote = mongoose.model('Vote', schema);
 
+/* 
+Routes
+*/
+
 // Render homepage.
 app.get('/', function(req, res) {
-    res.sendfile('index.html');
+	res.sendfile('index.html');
 });
 
 // Route for voting
 app.post('/vote', function(req, res) {
-    var field = [{name: req.body.name}];
+	var field = [{name: req.body.name}];
 
-    var newVote = new Vote(field[0]);
-    
-    newVote.save(function(err, data) {
-        console.log('Saved');
-    });
+	var newVote = new Vote(field[0]);
+	
+	newVote.save(function(err, data) {
+		console.log('Saved');
+	});
 
-    Vote.aggregate(
-        
-        [{ "$group": {
-            "_id": "$name",
-            "total_vote": { "$sum": 1 }
-        }}],
+	Vote.aggregate(
+		
+		[{ "$group": {
+			"_id": "$name",
+			"total_vote": { "$sum": 1 }
+		}}],
 
-        function(err, results) {
-            if (err) throw err;
-            console.log(results);
-            req.io.sockets.emit('vote', results);
-        }
-        );
+		function(err, results) {
+			if (err) throw err;
+			console.log(results);
+			req.io.sockets.emit('vote', results);
+		}
+		);
 
-    res.send({'message': 'Successfully added.'});
+	res.send({'message': 'Successfully added.'});
 });
 
 app.get('/data', function(req, res) {
-    Vote.find().exec(function(err, msgs) {
-        res.json(msgs);
-    });
+	Vote.find().exec(function(err, msgs) {
+		res.json(msgs);
+	});
 });
 
 /*
@@ -66,20 +70,19 @@ Socket.io Setting
 
 io.on('connection', function (socket) {
 
-    Vote.aggregate(
+	Vote.aggregate(
 
-        [{ "$group": {
-            "_id": "$name",
-            "total_vote": { "$sum": 1 }
-        }}],
+		[{ "$group": {
+			"_id": "$name",
+			"total_vote": { "$sum": 1 }
+		}}],
 
-        function(err, results) {
-            if (err) throw err;
+		function(err, results) {
+			if (err) throw err;
 
-            socket.emit('vote', results);
-        }
-        );
-
+			socket.emit('vote', results);
+		}
+	);
 });
 
 // Start
